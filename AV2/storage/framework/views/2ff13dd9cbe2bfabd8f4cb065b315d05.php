27bd19071d8e99d1fdacb1ce48ed9836e522c6b1
Luan Moreira de Menezes <125929128+LuanMez@users.noreply.github.com>
2024-11-27 19:02:31 -0300

    a

--- AV2/storage/framework/views/2ff13dd9cbe2bfabd8f4cb065b315d05.php
+++
@@ -1,9 +0,0 @@
-<!-- resources/views/layouts/nav.blade.php -->
-<nav>
-    <ul>
-        <li><a href="<?php echo e(url('/')); ?>">Home</a></li>
-        <li><a href="<?php echo e(url('/passageiros')); ?>">Passageiros</a></li>
-        <li><a href="<?php echo e(url('/outros')); ?>">Outros</a></li>
-    </ul>
-</nav>
-<?php /**PATH C:\Users\Luanm\aeroporto\resources\views/layouts/nav.blade.php ENDPATH**/ ?>
